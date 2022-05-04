8633d88c3fe6019313170b34455f43deaa8867e0
John Krasnay <john@krasnay.ca>
2022-05-04 16:03:38 -0700

    Implement search page

--- src/main/clojure/luhmann/webserver.clj
+++ src/main/clojure/luhmann/webserver.clj
@@ -1,14 +1,39 @@
 (ns luhmann.webserver
   (:require
+    [clojure.string :as string]
+    [hiccup2.core :as hiccup]
     [luhmann.core :as luhmann]
+    [luhmann.lucene :as lucene]
     [luhmann.watcher :as watcher]
     [org.httpkit.server :refer [as-channel run-server send!]]
-    [ring.middleware.file :refer [wrap-file]]))
+    [ring.middleware.file :refer [wrap-file]]
+    [ring.middleware.params :refer [wrap-params]]
+    ))
 
 
 (defonce server (atom nil))
 
 (defonce channels (atom #{}))
+
+
+(defn search-handler
+  [req]
+  (let [q (get-in req [:query-params "q"])
+        results (when-not (string/blank? q)
+                  (lucene/search q))]
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (str (hiccup/html
+                  [:html
+                   [:body
+                    [:h1 (str "Search results: '" q "'")]
+                    (if (seq results)
+                      (into [:div]
+                            (for [result results]
+                              [:div
+                               [:a {:href (:path result)} (:title result)]]))
+                      [:div "No results"])]]))}))
+
 
 (defn handler
   [req]
@@ -19,6 +44,9 @@
                                 (swap! channels conj ch))
                      :on-close (fn [ch _]
                                  (swap! channels disj ch))})
+
+    (= "/search" (:uri req))
+    (search-handler req)
 
     :else
     {:status 404
@@ -36,7 +64,8 @@
   [config]
   (stop)
   (reset! server (run-server (-> handler
-                                 (wrap-file (luhmann/site-dir)))
+                                 (wrap-file (luhmann/site-dir))
+                                 (wrap-params))
                              {:port (:port config)})))
 
 
