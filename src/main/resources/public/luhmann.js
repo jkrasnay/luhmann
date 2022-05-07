4f848aa1333d944250e19a47b10bc0c0f82b1a44
John Krasnay <john@krasnay.ca>
2022-05-07 11:49:23 -0700

    Focus search input with backslash

--- src/main/resources/public/luhmann.js
+++ src/main/resources/public/luhmann.js
@@ -1,3 +1,17 @@
 var socket = new WebSocket(location.origin.replace(/^http/, 'ws') + '/ws');
 socket.addEventListener('message', function (event) { window.location.reload(); });
 
+function focusSearch () {
+    var input = document.getElementsByName('q').item(0)
+    input.focus();
+    input.select();
+}
+
+function searchKeyListener (e) {
+    if (e.key === '\\') {
+        e.preventDefault();
+        focusSearch();
+    }
+}
+
+document.addEventListener('keydown', searchKeyListener)
